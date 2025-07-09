74ca72468e01af8db5edc419bc7e4a1f1f356a0b
Jyunao <165630043+Jyunao@users.noreply.github.com>
2025-07-09 22:53:05 +0900

    Update ai_server.py

--- ai_server.py
+++ ai_server.py
@@ -358,7 +358,7 @@ import time
 app = FastAPI()
 
 # 모델 파일 경로 & Google Drive 공유 링크
-MODEL_PATH = "congestion_model.pkl"
+MODEL_PATH = "congestion_model.bin"
 #DRIVE_URL = "https://drive.google.com/uc?id=13KwvWuWRXaDVQOU5-TeZMxFYPGE3KJcN" #pkl
 DRIVE_URL = "https://drive.google.com/uc?id=1EO8Xi0HYb-w8pK8o5GaCr4Edo066qkyX" #bin
 
